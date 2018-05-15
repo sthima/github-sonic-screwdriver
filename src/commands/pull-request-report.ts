f4bdc466333a3659f9ecc934e5562d4d40218528
Matheus Marchini <matheus@sthima.com>
2018-05-15 09:35:37 -0300

    commands: fix spacing on pull-requests

--- src/commands/pull-request-report.ts
+++ src/commands/pull-request-report.ts
@@ -5,27 +5,27 @@ import { PullRequestReport } from "../lib/pull-request";
 
 
 class PullRequestReportCommand extends GitHubCommand {
-    command = 'pull-request-report';
-    describe = '';
+  command = 'pull-request-report';
+  describe = '';
 
-    builder(yargs: Argv) {
-        return yargs
-            .wrap(90);
-    }
+  builder(yargs: Argv) {
+    return yargs
+        .wrap(90);
+  }
 
-    async handler(argv: any) {
-        let args = await argv as any;
-        const spinner = args.spinner;
-        const pull_request_report = new PullRequestReport ();
-        // try {
-            await pull_request_report.run();
-            spinner.succeed("Done");
-        // } catch (err) {
-        //     spinner.fail('oops')
-        //     return false;
-        // }
-        return true;
-    }
+  async handler(argv: any) {
+    let args = await argv as any;
+    const spinner = args.spinner;
+    const pull_request_report = new PullRequestReport ();
+    // try {
+      await pull_request_report.run();
+      spinner.succeed("Done");
+    // } catch (err) {
+    //     spinner.fail('oops')
+    //     return false;
+    // }
+    return true;
+  }
 }
 
 export = new PullRequestReportCommand();
